08211ab2e72befc06cb3cb13d2d2c1160c1c1a1c
goelparas <paras.20b0121211@abes.ac.in>
2023-06-14 15:45:18 +0530

    modify inovice

--- src/components/pdf/Invoice.jsx
+++ src/components/pdf/Invoice.jsx
@@ -146,6 +146,16 @@ const styles = StyleSheet.create({
         width: '100%',
 
     },
+    tableCellFirst: {
+        padding: 15,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        fontSize: 12,
+        width: '25%',
+        minHeight: 4,
+
+    },
     tableCell: {
         padding: 15,
         display: 'flex',
@@ -154,6 +164,15 @@ const styles = StyleSheet.create({
         fontSize: 12,
         width: '20%',
         minHeight: 4,
+
+    },
+    tableHeaderFirst: {
+        width: '25%',
+        backgroundColor: THEME_COLOUR,
+        color: 'white',
+        fontWeight: 'extrabold',
+        fontSize: 14,
+        padding: 10,
 
     },
     tableHeader: {
@@ -186,19 +205,31 @@ const styles = StyleSheet.create({
 
 const DummyData = {
     about: {
-        name: 'paras',
-        mobile: "7765546",
-        email: "p@gmail.com",
+        id:'1001',
+        name: 'Paras',
+        mobile: "987668895",
     },
     plan: {
-        name: 'student plan',
+        name: 'Student Package',
+        cardio:'NO',
         plan: 'Monthly',
-        price: '5000',
-        timespan: '5 Months '
+        registration: '5000',
+        fee: '1000 ',
+        planEnd: '10/10/23',
+        total:'6000',
+        userType:'New User'
     }
 
 }
-const Invoice = ({ invoice }) => (
+const Invoice = ({ invoice }) => {
+
+
+    const totalSum = (registrationfee, totalfee)=>{
+
+        return registrationfee+totalfee
+
+    }
+    return (
     <Document>
         <Page size="A4" style={styles.page}>
 
@@ -206,15 +237,15 @@ const Invoice = ({ invoice }) => (
                 <View style={styles.headerLeftcolumn}>
                     <Image src={Logo} style={styles.logo}></Image>
                     <Text style={styles.h3font}>Invoice to :</Text>
-                    <Text style={styles.pfont}>{DummyData.about.name}</Text>
-                    <Text style={styles.pfont}>{DummyData.about.mobile}</Text>
-                    <Text style={styles.pfont}>{DummyData.about.email}</Text>
+                    <Text style={styles.pfont}>UserID - {DummyData.about.id}</Text>
+                    <Text style={styles.pfont}>Name- {DummyData.about.name}</Text>
+                    <Text style={styles.pfont}>Mobile- {DummyData.about.mobile}</Text>
                 </View>
                 <View style={styles.headerRightcolumn}>
                     <Text style={styles.h1font}>INVOICE</Text>
                     <View style={styles.invoiceHeaderRow}>
                         <Text style={styles.h2font}>Invoice</Text>
-                        <Text style={styles.h2font}>0019</Text>
+                        <Text style={styles.h2font}>1001</Text>
                     </View>
                     <View style={styles.invoiceHeaderRow}>
                         <Text style={styles.h2font}>Date</Text>
@@ -227,90 +258,81 @@ const Invoice = ({ invoice }) => (
             <View style={styles.details}>
                 <View style={styles.table}>
                     <View style={styles.tableRow}>
-                        <View style={[styles.tableCell, styles.tableHeader]}>
+                        <View style={[styles.tableCellFirst, styles.tableHeaderFirst]}>
                             <Text>Subscription</Text>
                         </View>
                         <View style={[styles.tableCell, styles.tableHeader]}>
                             <Text>Type</Text>
                         </View>
                         <View style={[styles.tableCell, styles.tableHeader]}>
-                            <Text>Price</Text>
+                            <Text>Cardio</Text>
                         </View>
                         <View style={[styles.tableCell, styles.tableHeader]}>
-                            <Text>Time</Text>
+                            <Text>Reg.  Fee</Text>
                         </View>
                         <View style={[styles.tableCell, styles.tableHeader]}>
+                            <Text>Fees </Text>
+                        </View>
+                        {/* <View style={[styles.tableCell, styles.tableHeader]}>
                             <Text>Total</Text>
-                        </View>
+                        </View> */}
                     </View>
+                    
                     {/* {your data here  } */}
                     <View style={styles.tableRow}>
-                        <View style={styles.tableCell}>
+                        <View style={styles.tableCellFirst}>
                             <Text>{DummyData.plan.name}</Text>
                         </View>
                         <View style={styles.tableCell}>
                             <Text>{DummyData.plan.plan}</Text>
                         </View>
                         <View style={styles.tableCell}>
-                            <Text>{DummyData.plan.price}</Text>
+                            <Text>{DummyData.plan.cardio}</Text>
                         </View>
                         <View style={styles.tableCell}>
-                            <Text>{DummyData.plan.timespan}</Text>
+                            <Text>{DummyData.plan.registration}</Text>
                         </View>
                         <View style={styles.tableCell}>
-                            <Text>{DummyData.plan.price}</Text>
+                            <Text>{DummyData.plan.fee}</Text>
                         </View>
-                    </View>
-                    {/* {your data here  } */}
-                    <View style={styles.tableRow}>
-                        <View style={styles.tableCell}>
-                            <Text>{DummyData.plan.name}</Text>
-                        </View>
-                        <View style={styles.tableCell}>
-                            <Text>{DummyData.plan.plan}</Text>
-                        </View>
-                        <View style={styles.tableCell}>
-                            <Text>{DummyData.plan.price}</Text>
-                        </View>
-                        <View style={styles.tableCell}>
-                            <Text>{DummyData.plan.timespan}</Text>
-                        </View>
-                        <View style={styles.tableCell}>
-                            <Text>{DummyData.plan.price}</Text>
-                        </View>
+                        
+                        {/* <View style={styles.tableCell}>
+                            <Text>{DummyData.plan.total}</Text>
+                        </View> */}
                     </View>
 
                 </View>
             </View>
             <View style={styles.FlexRow}>
                 <View style={styles.headerLeftcolumn}>
-                    <Text style={styles.h3font}>Payment Info</Text>
+                    <Text style={styles.h3font}>More Details</Text>
                     <View style={styles.FlexRow}>
-                        <Text style={styles.pfont}>Account No</Text>
-                        <Text style={styles.pfont}> +012345678</Text>
+                        <Text style={styles.pfont}>Payment For </Text>
+                        <Text style={styles.pfont}>{DummyData.plan.userType}</Text>
                     </View>
                     <View style={styles.FlexRow}>
-                        <Text style={styles.pfont}>Name</Text>
-                        <Text style={styles.pfont}>Lorem Ipsum </Text>
+                        <Text style={styles.pfont}>Payment Type </Text>
+                        <Text style={styles.pfont}> UPI</Text>
                     </View>
+                    
                     <View style={styles.FlexRow}>
-                        <Text style={styles.pfont}>Bank Account</Text>
-                        <Text style={styles.pfont}>012345678</Text>
+                        <Text style={styles.pfont}>Plan Ends</Text>
+                        <Text style={styles.pfont}>10/10/23</Text>
                     </View>
 
                 </View>
                 <View style={styles.headerRightcolumn}>
                     <View style={styles.totalSectionRow}>
                         <Text style={styles.totalSectionFont}>Sub total</Text>
-                        <Text style={styles.totalSectionFont}> 20</Text>
+                        <Text style={styles.totalSectionFont}>6,000</Text>
                     </View>
-                    <View style={styles.totalSectionRow}>
-                        <Text style={styles.totalSectionFont}>Tax</Text>
-                        <Text style={styles.totalSectionFont}>10 </Text>
-                    </View>
+                    {/* <View style={styles.totalSectionRow}>
+                        <Text style={styles.totalSectionFont}>Registration Fees</Text>
+                        <Text style={styles.totalSectionFont}>100</Text>
+                    </View> */}
                     <View style={styles.totalSectionRowlast}>
                         <Text>Total</Text>
-                        <Text>100 </Text>
+                        <Text>{totalSum(5000, 1000)} </Text>
                     </View>
                 </View>
             </View>
@@ -324,6 +346,6 @@ const Invoice = ({ invoice }) => (
         </Page>
     </Document>
 );
-
+}
 
 export default Invoice;
