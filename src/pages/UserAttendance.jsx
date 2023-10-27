8263962bb55e2b55941557718ebb69d6d0bf9b43
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2023-10-28 00:50:14 +0530

    fixed bug in attendance form home page

--- src/pages/UserAttendance.jsx
+++ src/pages/UserAttendance.jsx
@@ -76,34 +76,32 @@ function UserAttendance() {
                 />
               </svg>
             </Link>
-            <div className="px-4 py-2 whitespace-nowrap text-sm md:text-base text-gray-100 flex justify-center text-center mb-2 -mt-5 ">
+            <div className="flex flex-col justify-center items-center">
+            <div className="px-4 py-2 whitespace-nowrap text-sm md:text-base text-gray-100 flex justify-center text-center mb-1">
               {userData.photoURL ? (
                 <a
                   href={userData.photoURL}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <div class="rounded-full h-20 w-20 justify-evenly items-center mb-10">
+                  <div class="rounded-full h-20 w-20 justify-evenly items-center">
                     <img
                       src={userData.photoURL}
                       alt="image"
                       class="h-full w-full rounded-full border object-cover"
                     />
-                    <h1 className="text-xl font-semibold text-orange-500 mt-2">
-                      {userData.name}
-                    </h1>
                   </div>
                 </a>
               ) : (
                 <p className="text-xs text-red-400 p-4">Not found</p>
               )}
             </div>
-            {/* <div className="flex items-center mb-6">
-              <FaUserCircle className="text-3xl text-gray-300 mr-2" />
+            <div className="flex items-center mb-6">
               <h1 className="text-xl font-semibold text-orange-500">
                 {userData.name}
               </h1>
-            </div> */}
+            </div>
+            </div>
             <p className="text-gray-300 mb-6">User ID: {userData.id}</p>
             <div className="flex items-center mb-6">
               <p className="text-gray-200 mr-2">Status:</p>
