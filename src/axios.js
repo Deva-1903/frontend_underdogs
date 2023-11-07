e124505a8f03da56a12b6f1d4cb8f124b3015137
Deva <97371915+Deva-1903@users.noreply.github.com>
2023-11-07 23:08:09 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = "https://api.underdogsfitness.in";
 
 const axiosInstance = axios.create({
   baseURL: BACKEND_URL,
