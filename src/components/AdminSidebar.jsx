ba79d40a81d0d4451ec805f73220e08f9ca1e11c
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2024-07-19 14:57:01 +0530

    updated logo

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -16,6 +16,7 @@ import { logout, reset } from "../features/auth/authSlice";
 import { RiAdminFill } from "react-icons/ri";
 import { useSelector, useDispatch } from "react-redux";
 import { GiMoneyStack } from "react-icons/gi";
+import { FaClipboardQuestion } from "react-icons/fa6";
 
 function AdminSidebar() {
   const navigate = useNavigate();
@@ -118,11 +119,20 @@ function AdminSidebar() {
         </>
       ),
     },
+    {
+      Name: "Contact Forms",
+      links: "/admin/get/contactforms",
+      child: (
+        <>
+          <FaClipboardQuestion />
+        </>
+      ),
+    },
   ];
 
   return (
-    <section className="flex w-full bg-black">
-      <div className="bg-black min-h-screen duration-500 text-gray-100 px-4 flex flex-col items-center">
+    <section className="flex w-full bg-black overflow-y-scroll">
+      <div className="bg-black h-screen duration-500 text-gray-100 px-4 flex flex-col items-center">
         <div className="py-2 lg:py-2 px-6 lg:px-6 flex items-center">
           <img
             src={logo}
